14b1c90099d2b8dac5dfec00c1ae4809f6ffb853
Bryan Cora <corabj@mail.uc.edu>
2024-10-14 13:12:12 -0400

    char count

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from collections import Counter
+
 def main():
     with open("books/frankenstein.txt") as f:
         print(text := f.read())
     print(f"the word count is: {word_count(text)}")
+    print(char_count(text))
 
 
 def word_count(text: str):
     return len(text.split())
+
+
+def char_count(text: str):
+    c = Counter(text.lower())
+    return dict(c)
+
+
+
 if __name__ == "__main__":
     main()
